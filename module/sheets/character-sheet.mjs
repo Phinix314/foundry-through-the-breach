b3bed13b8bd306f7f84dde4082575283fac8aa64
Phinix <phinix271828@gmail.com>
2026-04-15 21:32:21 +0200

    Adding Style and background.

--- module/sheets/character-sheet.mjs
+++ module/sheets/character-sheet.mjs
@@ -3,7 +3,7 @@ const SYSTEM_ID = "through-the-breach";
 export class TTBCharacterSheet extends ActorSheet {
     static get defaultOptions() {
         return foundry.utils.mergeObject(super.defaultOptions, {
-            classes: [SYSTEM_ID, "sheet", "actor"],
+            classes: [SYSTEM_ID, "sheet", "actor", "ttb-character-sheet"],
             template: `systems/${SYSTEM_ID}/templates/actor/character-sheet.hbs`,
             width: 600,
             height: 400,
